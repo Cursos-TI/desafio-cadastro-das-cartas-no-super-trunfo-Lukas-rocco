d8caaaeffa2ae1e694be3c1f4c0672c393143308
Lukas-rocco <lukas.rocco16@gmail.com>
2025-02-21 15:19:57 +0000

    Finalizado desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -27,7 +27,7 @@
    //PIB: 699.28 bilhões de reais
    // Número de Pontos Turísticos: 50
 
-int main() {
+ int main() {
 
  char estado1[25];
  char codigo1 [4];
@@ -37,8 +37,13 @@ int main() {
  float pib1;
  int pontos1;
 
-
-
+ char estado2 [20];
+ char codigo2[4];
+ char cidade2[19];
+ int pop2;
+ float area2;
+ float pib2 ;
+ int pontos2;
 
     printf("Desafio do Super Trunfo!\n");
  
@@ -67,6 +72,33 @@ scanf("%f", &pib1);
 printf("Quantos pontos turísticos possui? ");
 scanf("%d",&pontos1);
 
+printf("Agora a segunda carta\n");
+
+printf("Qual estado? ");
+scanf("%s", estado2);
+
+printf("Qual o codigo? ");
+scanf("%s", codigo2);
+
+printf("Qual a cidade? ");
+scanf("%s", cidade2);
+
+printf("Qual população da cidade? ");
+scanf("%d", &pop2);
+
+printf("Qual a area dessa cidade? ");
+scanf("%f", &area2);
+
+printf("Qual o pib da cidade? ");
+scanf("%f", &pib2);
+
+printf("Quantos pontos turísticos tem? ");
+scanf("%d", &pontos2);
+
+
+
+
+
 printf("Carta 1:\n");
 printf("Estado: %s \n", estado1);
 printf("Codigo: %s \n",codigo1);
@@ -76,6 +108,14 @@ printf("Área: %.2f Km²\n",area1);
 printf("PIB: %.2f bilhões de reais\n",pib1);
 printf("Pontos turísticos: %d \n",pontos1);
 
+printf("Carta 2:\n");
+printf("Estado: %s \n", estado2);
+printf("Codigo: %s \n",codigo2);
+printf("Cidade: %s \n",cidade2);
+printf("População: %d \n",pop2);
+printf("Área: %.2f Km²\n",area2);
+printf("PIB: %.2f bilhões de reais\n",pib2);
+printf("Pontos turísticos: %d \n",pontos2);
 
 
 
